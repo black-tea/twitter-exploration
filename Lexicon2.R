d0dc3e338942959a520087ff81daa290f3c71b2b
black-tea <timothy.c.black@gmail.com>
2017-11-05 22:47:31 -0800

    Added Lexicon Dictionaries to Analysis

--- Lexicon2.R
+++ Lexicon2.R
@@ -4,28 +4,31 @@ library(dplyr)
 library(lubridate) # with_tz function
 library(reshape2) # with melt() function
 library(scales) # with date_breaks() function
+library(tidyr) 
 
 ### Data Prep
 # Load Data
 dot_df <- read.csv('C:/Users/Tim/Documents/GitHub/twitter-exploration/twitterscraper_data.csv')
 
-# Clean data, Calculate Sentiment, & Add columns
+# Clean
 dot_df$text <- sapply(dot_df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
-dot_sentiment <- get_nrc_sentiment(dot_df$text)
-tweets <- cbind(dot_df, dot_sentiment)
 
-### Sentiment Totals
-# Calculate Sentiment Totals and Plot
-sentimentTotals <- data.frame(colSums(dot_sentiment))
-names(sentimentTotals) <- "count"
-sentimentTotals <- cbind("sentiment" = rownames(sentimentTotals), sentimentTotals)
-rownames(sentimentTotals) <- NULL
+# Calculate Sentiment Scores and Append to df
+bing_sentiment <- get_sentiment(dot_df$text, method="bing")
+afinn_sentiment <- get_sentiment(dot_df$text, method="afinn")
+nrc_sentiment <- get_nrc_sentiment(dot_df$text)
+tweets <- cbind(dot_df, nrc_sentiment, bing_sentiment, afinn_sentiment)
 
-# Plot Sentiment Totals
-ggplot(data = sentimentTotals, aes(x = sentiment, y = count)) +
-  geom_bar(aes(fill = sentiment), stat = "identity") +
-  theme(legend.position = "none") +
-  xlab("Sentiment") + ylab("Total Count") + ggtitle("Total Sentiment Score for All Tweets")
+# Bing & Afinn Lexicons: Separate Positive / Absolute value of negative scores
+tweets <- tweets %>%
+  mutate(
+    bing.pos = if_else(bing_sentiment > 0, bing_sentiment, as.integer(0)),
+    bing.neg = if_else(bing_sentiment < 0, abs(bing_sentiment), as.integer(0))
+  ) %>%
+  mutate(
+    afinn.pos = if_else(afinn_sentiment > 0, afinn_sentiment, as.integer(0)),
+    afinn.neg = if_else(afinn_sentiment < 0, abs(afinn_sentiment), as.integer(0))
+  )
 
 ### Sentiment Over Time
 # Calculate Sentiment over Time
@@ -34,12 +37,12 @@ monthlysentiment <- tweets %>%
   # Group by Month
   group_by(timestamp = cut(timestamp, breaks="2 weeks")) %>%
   # For each month, sum totals of each emotion
-  summarise(negative = sum(negative),
-            positive = sum(positive)) %>%#,
-            #anger = sum(anger),
-            #anticipation = sum(anticipation),
-            #disgust = sum(disgust),
-            #fear = sum(fear),
+  summarise(#nrc.neg.sum = sum(negative),
+            #nrc.pos.sum = sum(positive),
+            bing.neg.sum = sum(bing.neg),
+            bing.pos.sum = sum(bing.pos)) %>%
+            #afinn.pos.sum = sum(afinn.pos),
+            #affin.neg.sum = sum(afinn.neg)) %>%
             #joy = sum(joy),
             #sadness = sum(sadness),
             #surprise = sum(surprise),
@@ -69,10 +72,20 @@ ggplot(data = monthlysentiment, aes(x = as.Date(month), y = count, group = senti
   # Full Reversal Announced w/ Garcetti
   geom_vline(aes(xintercept = as.numeric(as.Date("2017-10-18"))), 
              linetype=4, col = "black") +
-  annotate("text",x = as.Date("2017-10-18"),y=425, label= "Full PdR Reversal Announced\n", size=3, angle=90) +
+  annotate("text",x = as.Date("2017-10-18"),y=275, label= "Full PdR Reversal Announced\n", size=3, angle=90) +
   # Culver / Jefferson Westbound Travel Lane Reversal Announced
   geom_vline(aes(xintercept = as.numeric(as.Date("2017-10-2"))), 
              linetype=4, col = "black") +
-  annotate("text",x = as.Date("2017-10-2"),y=425, label= "Culver/Jefferson WBound Reversal\n", size=3, angle=90) +  
+  annotate("text",x = as.Date("2017-10-26"),y=275, label= "Culver/Jefferson WBound Reversal\n", size=3, angle=90) +  
+  # Culver / Jefferson Westbound Travel Lane Reversal Announced
+  geom_vline(aes(xintercept = as.numeric(as.Date("2016-10-17"))), 
+             linetype=4, col = "black") +
+  annotate("text",x = as.Date("2016-10-17"),y=250, label= "Trump Star Vandalized\n", size=3, angle=90) + 
   ylab("Sentiment Count") + 
   ggtitle("Tweet Sentiments Involving 'LADOT'")
+
+# Let's explore one of the local peaks between 10/17 & 10/31
+explore <- tweets %>%
+  select(bing.pos, text,timestamp) %>%
+  filter(timestamp > "2016-10-17" & timestamp < "2016-10-31")
+  
